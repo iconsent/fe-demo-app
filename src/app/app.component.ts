c412a884ae42c591da121e6fca19cb969cad604d
Vivek Srivastav <vsrivastav@gmail.com>
2021-04-03 23:25:10 -0400

    Initial checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import {PropertiesService} from './properties.service';
+import {MatTableDataSource} from '@angular/material/table';
+export interface Property {
+  key: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fe-app-demo';
+export class AppComponent implements OnInit {
+
+  title = 'fe-demo-app';
+  error = false;
+  data: any;
+
+  constructor(public svc: PropertiesService) { }
+
+
+  ngOnInit(): void {
+    this.svc.getProperties().subscribe(
+      (data) => {
+        this.error = false;
+        console.log(data);
+        this.data = data;
+      },
+      error => {
+        this.error = true;
+        console.log(error);
+      }
+    );
+  }
+  
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://springboot-demo:8080'
 };
